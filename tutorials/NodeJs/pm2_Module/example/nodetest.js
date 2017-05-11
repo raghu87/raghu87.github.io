f3a3dc29633c8407b15e87619d5d979236f7e92c
raghu87 <raghuachar3187@gmail.com>
2017-05-11 13:00:34 +0530

    added mkdir and storing file feature

--- tutorials/NodeJs/pm2_Module/example/nodetest.js
+++ tutorials/NodeJs/pm2_Module/example/nodetest.js
@@ -1,9 +1,28 @@
+var homePath = "";
+if(process.env && process.env['HOME']) {
+  homePath = process.env['HOME'];
+}
 console.log("inside nodetest ",new Date(),Date.now());
 var fs = require('fs');
-fs.writeFile("/home/raghu87/Desktop/servicetest/test"+Date.now(), "Hey there!", function(err) {
-    if(err) {
-        return console.log(err);
-    }
+var fx = require('mkdir-recursive');
+var transDir = homePath+"/Desktop/servicetest";
+var tdExists = fs.existsSync(transDir);
+if(tdExists) {
+  console.log(transDir + ' directory present');
+  storeFile();
+} else {
+  console.log(transDir + ' directory not present');
+  fx.mkdir(transDir, function(err) {
+    console.log('directory created');
+    storeFile();
+  });
+}
 
+function storeFile() {
+  fs.writeFile(transDir+"/test"+Date.now(), "Hey there!", function(err) {
+    if(err) {
+      return console.log(err);
+    }
     console.log("The file was saved!");
-}); 
+  }); 
+}
